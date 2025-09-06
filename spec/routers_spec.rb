feed0c8e1af35f3ff05b91781964d0f65b93bf27
Jordan Scales <scalesjordan@gmail.com>
2025-09-05 22:52:23 -0400

    remove parens because ruby

--- spec/routers_spec.rb
+++ spec/routers_spec.rb
@@ -18,44 +18,44 @@ require 'rack/builder'
 
     it 'responds to GET /' do
       get '/'
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq('You got here by: /')
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'You got here by: /'
     end
 
     it 'responds to GET /welcome/to/my/site' do
       get '/welcome/to/my/site'
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq('You got here by: /welcome/to/my/site')
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'You got here by: /welcome/to/my/site'
     end
 
     it 'responds to GET /nuts/:number' do
       get '/nuts/7'
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq('You got here by: /nuts/7')
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'You got here by: /nuts/7'
     end
 
     it 'responds to GET /gorp/:anything' do
       get '/gorp/jordan'
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq('You got here by: /gorp/jordan')
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'You got here by: /gorp/jordan'
     end
 
     it 'responds to GET /nuts/:number/:anything' do
       get '/nuts/7/jordan'
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq('You got here by: /nuts/7/jordan')
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq 'You got here by: /nuts/7/jordan'
     end
 
     it 'returns 404 when passing a string and expecting a number' do
       get '/nuts/hello/jordan'
-      expect(last_response.status).to eq(404)
-      expect(last_response.body).to eq('404 Not Found')
+      expect(last_response.status).to eq 404
+      expect(last_response.body).to eq '404 Not Found'
     end
 
     it 'returns 404 for unknown routes' do
       get '/path/with/multiple/segments'
-      expect(last_response.status).to eq(404)
-      expect(last_response.body).to eq('404 Not Found')
+      expect(last_response.status).to eq 404
+      expect(last_response.body).to eq '404 Not Found'
     end
   end
 end
