40b85f60d05d6320447c58f520849f20b7461136
Jordan Scales <scalesjordan@gmail.com>
2025-09-06 10:43:14 -0400

    rubocop-rake and rubocop-rspec

--- Gemfile
+++ Gemfile
@@ -3,14 +3,11 @@
 source 'https://rubygems.org'
 
 gem 'puma', '~> 6.6'
-gem 'rackup', '~> 2.2'
-
-gem 'rdoc', '~> 6.14'
-
-gem 'rspec', '~> 3.13'
-
 gem 'rack-test', '~> 2.2'
-
-gem 'rubocop', '~> 1.0', require: false
-
+gem 'rackup', '~> 2.2'
 gem 'rake', '~> 13.3'
+gem 'rdoc', '~> 6.14'
+gem 'rspec', '~> 3.13'
+gem 'rubocop', '~> 1.0', require: false
+gem 'rubocop-rake', '~> 0.7.1'
+gem 'rubocop-rspec', '~> 3.7'
--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -13,51 +13,83 @@ require 'rack/builder'
       Rack::Builder.parse_file(ru_path)
     end
 
-    it 'responds to GET /' do
+    it 'responds to GET / with 200' do
       get '/'
       expect(last_response.status).to eq 200
+    end
+
+    it 'responds to GET / with correct response' do
+      get '/'
       expect(last_response.body).to eq 'You got here by: /'
     end
 
-    it 'can identify itself with /router' do
+    it 'can identify itself with /router with 200' do
       get '/router'
       expect(last_response.status).to eq 200
+    end
+
+    it 'can identify itself with /router with correct response' do
+      get '/router'
       expect(last_response.body).to eq "Routing strategy: #{name}"
     end
 
-    it 'responds to GET /welcome/to/my/site' do
+    it 'responds to GET /welcome/to/my/site with 200' do
       get '/welcome/to/my/site'
       expect(last_response.status).to eq 200
+    end
+
+    it 'responds to GET /welcome/to/my/site with correct response' do
+      get '/welcome/to/my/site'
       expect(last_response.body).to eq 'You got here by: /welcome/to/my/site'
     end
 
-    it 'responds to GET /nuts/:number' do
+    it 'responds to GET /nuts/:number with 200' do
       get '/nuts/7'
       expect(last_response.status).to eq 200
+    end
+
+    it 'responds to GET /nuts/:number with correct response' do
+      get '/nuts/7'
       expect(last_response.body).to eq 'You got here by: /nuts/7'
     end
 
-    it 'responds to GET /gorp/:anything' do
+    it 'responds to GET /gorp/:anything with 200' do
       get '/gorp/jordan'
       expect(last_response.status).to eq 200
+    end
+
+    it 'responds to GET /gorp/:anything with correct response' do
+      get '/gorp/jordan'
       expect(last_response.body).to eq 'You got here by: /gorp/jordan'
     end
 
-    it 'responds to GET /nuts/:number/:anything' do
+    it 'responds to GET /nuts/:number/:anything with 200' do
       get '/nuts/7/jordan'
       expect(last_response.status).to eq 200
+    end
+
+    it 'responds to GET /nuts/:number/:anything with correct response' do
+      get '/nuts/7/jordan'
       expect(last_response.body).to eq 'You got here by: /nuts/7/jordan'
     end
 
-    it 'returns 404 when passing a string and expecting a number' do
+    it 'returns 404 when passing a string and expecting a number with 200' do
       get '/nuts/hello/jordan'
       expect(last_response.status).to eq 404
+    end
+
+    it 'returns 404 when passing a string and expecting a number with correct response' do
+      get '/nuts/hello/jordan'
       expect(last_response.body).to eq '404 Not Found'
     end
 
     it 'returns 404 for unknown routes' do
       get '/path/with/multiple/segments'
       expect(last_response.status).to eq 404
+    end
+
+    it 'returns 404 for unknown routes with correct response' do
+      get '/path/with/multiple/segments'
       expect(last_response.body).to eq '404 Not Found'
     end
   end
